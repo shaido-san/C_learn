a4ce4d77d390c96849b160b3ef523509eb2dd92c
saito4 <hiroto.saitou1201@gmail.com>
2025-04-24 19:50:41 +0900

    四則演算

---
+++ C_file/calculation.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 10 + 3);
+    printf("%d\n", 10 - 3);
+    printf("%d\n", 10 * 3);
+    printf("%d\n", 10 / 3);
+    printf("%d\n", 10 % 3);
+    return 0;
+}
