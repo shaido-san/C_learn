e8728c42493171cbd0846cfa02f373e3ac893869
saito4 <hiroto.saitou1201@gmail.com>
2025-04-29 23:22:25 +0900

    8進数

--- C_file/calculation2.c
+++ C_file/calculation2.c
@@ -6,5 +6,7 @@ int main(void)
     printf("%f\n", 10.0 - 3.0);
     printf("%f\n", 10.0 * 3.0);
     printf("%f\n", 10.0 / 3.0);
+    printf("%d / %d = %d ... %d\n", 40,13,40 / 13, 40 % 13);
+
     return 0;
 }
