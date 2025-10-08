9b6cafe28aa640be0376733ce9fd4bd8bf01ee08
saito4 <hiroto.saitou1201@gmail.com>
2025-10-08 16:50:58 +0900

    case_default

---
+++ C_file/part8/case2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int no;
+    scanf("%d",&no);
+    switch (no)
+    {
+    case 1:
+    case 3:
+    case 4:
+       printf("男性\n");
+       break;
+    case 2:
+       printf("女性\n");
+       break;
+    default:
+       printf("そんな番号の人はいない\n");
+       break;
+    }
+    return 0;
+}
