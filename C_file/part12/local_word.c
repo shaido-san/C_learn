fbff1643215591ed4b8ca1b44a9a39eeecf563b1
saito4 <hiroto.saitou1201@gmail.com>
2025-11-04 17:43:32 +0900

    local

---
+++ C_file/part12/local_word.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int countfunc(void);
+
+int main(void)
+{
+    countfunc();
+    countfunc();
+    countfunc();
+    return 0;
+}
+
+int countfunc(void)
+{
+    int count = 0;
+    count++;
+    printf("%d\n",count);
+    return count;
+}
