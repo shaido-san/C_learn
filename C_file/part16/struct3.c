7e65b7d4f730c33d3335c3b62cbe9660adcb5e2a
saito4 <hiroto.saitou1201@gmail.com>
2025-12-09 20:38:38 +0900

    struct_learn

---
+++ C_file/part16/struct3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    int year;
+    int clas;
+    int number;
+    char name[64];
+    double stature;
+    double weight;
+} student;
+
+void student_print(student data);
+
+int main(void)
+{
+    student data;
+
+    data.year = 3;
+    data.clas = 4;
+    strcpy(data.name, "MARIO");
+    data.stature = 175.6;
+    data.weight = 75.4;
+
+    student_print(data);
+    return 0;
+}
+
+void student_print(student data)
+{
+    printf("[学年]:%d\n",data.year);
+    printf("[クラス]:%d\n",data.clas);
+    printf("[出席番号]:%d\n",data.number);
+    printf("[名前]:%s\n",data.name);
+    printf("[身長]:%f\n",data.stature);
+    printf("[身長]:%f\n",data.weight);
+}
